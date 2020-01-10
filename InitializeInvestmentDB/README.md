4422d918aa242c0d90122f3856f1a5935034ff8b
Unknown <dzamero@gmail.com>
2020-01-09 20:44:29 -0500

    Moved the csv data load function under the LoadCSVDataIntoDyamoDB folder.
    Checked in the functionality to scan the holdings and call rest api to gather couple months worth of records for each ticker and load it to the new DynamoDB table

---
+++ InitializeInvestmentDB/README.md
@@ -0,0 +1,4 @@
+1 - Create a new lambda function with a security role that have access to Dynamodb
+2 - Zip this folder and upload it to the lamda function
+3 - Create a new DynamoDB table called TickerTimeSeries
+4 - Expose this lamda function as a service so we can trigger it before the application startup
---
+++ InitializeInvestmentDB/lambda_function.py
@@ -0,0 +1,62 @@
+
+import json
+from urllib.parse import unquote_plus
+import boto3
+import csv
+import sys
+import uuid
+import requests
+
+from botocore.exceptions import ClientError
+
+print('Loading function')
+
+def lambda_handler(event, context):
+    region='us-east-1'
+    recList=[]
+    
+    try:
+        tickerList = ['aapl', 'goog'];
+        dyndb = boto3.client('dynamodb', region_name=region)
+        dyndbResource = boto3.resource('dynamodb')
+        
+        investmentPortfolioTable = dyndbResource.Table('InvestmentPortfolio')
+        response = investmentPortfolioTable.scan()
+        items = response['Items']
+       
+        uniqueTickers = [];
+        for i in items:
+            if(i["ticker"] not in uniqueTickers):
+                 uniqueTickers.append(i["ticker"]);
+       
+        for item in uniqueTickers:
+            print(item)
+            res = requests.get('https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol='+item+'&apikey=W03G1YF4WE1KCNQ5')
+            resJson = res.json()
+            
+            for dt in resJson["Time Series (Daily)"]:
+                print(resJson["Time Series (Daily)"][dt]["4. close"]);
+                
+                response = dyndb.put_item(
+                    TableName='TickerTimeSeries',
+                    Item={
+                    'tickerName': {'S': resJson["Meta Data"]["2. Symbol"]},
+                    'tickerPrice' : {'N':resJson["Time Series (Daily)"][dt]["4. close"]},
+                    'tickerDate' : {'S':dt}
+                    }
+                )
+            print('Put succeeded:')
+        # TODO implement
+        return {
+            'statusCode': 200,
+            'body': json.dumps('Historical Stock Data is Successfully loaded in the DynamoDB!')
+            }
+    
+    except ClientError as err:
+       print("Unexpected error:", str(err))
+       return {
+            'statusCode': 500,
+            'body': json.dumps('Historical Stock Data load Failed!')
+            }
+
+
